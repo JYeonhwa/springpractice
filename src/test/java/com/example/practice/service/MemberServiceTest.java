ce7a73fcb07ca91bde7796479b2cdbef60d80ac0
JeongYeonHwa <laboratory6889@gmail.com>
2023-02-22 17:16:28 +0900

    practice init

---
+++ src/test/java/com/example/practice/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.practice.service;
+
+import com.example.practice.domain.Member;
+import com.example.practice.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public  void  afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void MemberDuplicateException() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
